3d0946c8c85a9821f6b522f876c91ae39dc83681
Tyler Wolfe-Adam <t.wolfeadam@gmail.com>
2020-10-09 20:39:16 -0500

    Update Lorenz
    + move_figure function
    + slider for plot update
    * code cleanup

--- Examples/Lorenz Weather Model - Pypeline/plotter.py
+++ Examples/Lorenz Weather Model - Pypeline/plotter.py
@@ -1,26 +1,30 @@
+import matplotlib
 from matplotlib import use
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from collections import deque
 
-
-xs = deque(maxlen=500)
-ys = deque(maxlen=500)
-zs = deque(maxlen=500)
-
 # Provides better graph drawing (e.g., moving chart w/o locking thread)
 # But requires PyQt5 to be installed
 #use('Qt5Agg')
 
+# creation of figure
 fig = plt.figure()
 fig.canvas.set_window_title("Lorenz Weather Model - Stock plotting")
 
+# limit size to the same as default size of datasets in AL model
+xs = deque(maxlen=500)
+ys = deque(maxlen=500)
+zs = deque(maxlen=500)
+
+# create 3d plot
 ax = fig.add_subplot(111, projection="3d")
 ax.set(xlim=[-25, 25], ylim=[-30, 30], zlim=[0, 50])
 ax.set_xlabel("X", color="goldenrod", weight="bold")
 ax.set_ylabel("Y", color="orangered", weight="bold")
 ax.set_zlabel("Z", color="dodgerblue", weight="bold")
 
+# initial drawing and showing
 fig.canvas.draw()
 bg = fig.canvas.copy_from_bbox(ax.bbox)
 plt.show(block=False)
@@ -30,15 +34,29 @@ line_style = '-'
 
 line3d, = ax.plot(xs, ys, zs, line_color + line_style)
 
-#### Function called from AL:
+#### Functions called from AL:
 
-def set_inputs(S, R, B):
+def set_inputs(S: float, R: float, B: float) -> None:
+    """ Update the plot's title based on the inputs of the AL model """
     ax.set_title(f"S={S:.2f}", loc='left', color="goldenrod")
     ax.set_title(f"R={R:.2f}", loc='center', color="orangered")
     ax.set_title(f"B={B:.2f}", loc='right', color="dodgerblue")
     plt.tight_layout()
 
-def append(x, y, z):
+def move_figure(x, y):
+    """ Move figure's upper left corner to pixel (x, y) """
+    backend = matplotlib.get_backend()
+    if backend == 'TkAgg':
+        fig.canvas.manager.window.wm_geometry("+%d+%d" % (x, y))
+    elif backend == 'WXAgg':
+        fig.canvas.manager.window.SetPosition((x, y))
+    else:
+        # This works for QT and GTK
+        # You can also use window.setGeometry
+        fig.canvas.manager.window.move(x, y)
+    
+def append(x: float, y: float, z: float) -> None:
+    """ Append the provided point to the datasets and redraw the plot """
     xs.append(x)
     ys.append(y)
     zs.append(z)
