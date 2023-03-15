2bfd23dc611285efd0db352a68561a5b20ef56ab
t.wolfeadam <t.wolfeadam@gmail.com>
2023-03-15 15:47:36 -0500

    Remove unused function from python helper

--- Examples/BasicModels/Initializing and Exporting with JSON/helper.py
+++ Examples/BasicModels/Initializing and Exporting with JSON/helper.py
@@ -28,28 +28,10 @@ def get_latest_data() -> str:
 
     return json.dumps(data)
 
+
 def save_latest_data(filename: str) -> None:
     """Get the "latest" data (e.g., from our internal company service),
         and save it to the specified local JSON file"""
     output = get_latest_data()
     with open(filename, "w") as outfile:
         outfile.write(output)
-    
-def linear_regression(x, y):
-    # Calculate the mean of X and Y
-    x_mean = statistics.mean(x)
-    y_mean = statistics.mean(y)
-
-    # Calculate the differences between each X and the mean of X, and each Y and the mean of Y
-    x_diff = [i - x_mean for i in x]
-    y_diff = [i - y_mean for i in y]
-
-    # Calculate the slope (m)
-    m = sum([x_diff[i] * y_diff[i] for i in range(len(x))]) / sum([i**2 for i in x_diff])
-
-    # Calculate the intercept (b)
-    b = y_mean - m * x_mean
-
-    # Print the results
-    print("Slope (m):", m)
-    print("Intercept (b):", b)
